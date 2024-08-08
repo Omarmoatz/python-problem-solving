db2f576d34005a3adf9ab7d1c61b1fe2725d7e89
Omarmoatz <omar.raouf.002@gmail.com>
2024-08-08 19:35:31 +0300

    add pretty table

---
+++ pretty_table.py
@@ -0,0 +1,25 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names= ['Place', 'Team', 'Play', 'Win', 'Points'] 
+
+teams = [
+    ['Svillia', 21, 10, 35],
+    ['Atletco Madred', 21, 9, 31],
+    ['Real Madred', 21, 20, 51],
+    ['Valencia', 21, 12, 40],
+    ['FC Barcelona', 21, 15, 46],
+]
+
+# def sorted_teams(teams):
+#     return teams[3]
+
+teams.sort(key= lambda teams: teams[3] , reverse=True)
+
+for index, team in enumerate(teams):
+    team.insert(0, index+1)
+    table.add_row(team)
+
+print(table)
+
